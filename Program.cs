725f062de2b93c0ebc5b7831b99ded44a74d8912
Hugo Vettore <vettore.h@gmail.com>
2024-04-26 13:09:36 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using Newtonsoft.Json;
+
+namespace WeatherLogApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
